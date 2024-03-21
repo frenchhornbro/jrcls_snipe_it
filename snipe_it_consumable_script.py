0cc326396c841049c6de5529dd647b87d19f54c3
Hyrum Durfee <htdurfee@gmail.com>
2024-03-21 16:45:47 -0600

    Added a 20-second delay between each email being sent

--- snipe_it_consumable_script.py
+++ snipe_it_consumable_script.py
@@ -1,3 +1,4 @@
+import time
 from http_handler import HTTPHandler
 from asset_creator import AssetCreator
 from asset import Asset
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     assetCreator:AssetCreator = AssetCreator(handler.getAsset())
     assets:list[Asset] = assetCreator.createAssets()
     asset: Asset
-    logger.log("\n")
+    logger.log("\n", True, True)
     logger.log("Begin script", True)
     for asset in assets:
         if asset.get_current_qty() <= asset.get_reorder_at_qty():
@@ -19,6 +20,7 @@ if __name__ == '__main__':
                     logger.log("Asset was ordered")
                     emailer:Emailer = Emailer(asset.email_msg())
                     emailer.run()
+                    time.sleep(20) #Without this Jira freaks out and can take up to 30 minutes to actually receive the email and run its end of the script
             elif asset.get_order() != "ORDERED":
                 if (asset.set_order("")):
                     logger.log(f"Asset order tag was cleared of the following unnecessary text: \"{asset.get_order()}\"")
