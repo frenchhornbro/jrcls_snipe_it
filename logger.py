43c920b3688c75f3f60e72c8c45a13ca857c74da
Hyrum Durfee <htdurfee@gmail.com>
2024-04-22 17:59:25 -0600

    Weekly log handles creation, updates, and deletion

--- logger.py
+++ logger.py
@@ -7,9 +7,12 @@ class Logger:
         self.fileName = ""
 
     def log(self, input, title:bool=False, newline:bool=False) -> None:
+        logFolder:Path = Path("./log")
+        logFile:Path = Path("./log/log.txt")       #need to cd to this directory in the bat file for this to work
         input = str(input)
-        fileName = "./log/log.txt"                  #need to create a "log" directory in the current directory (also need to cd to this directory for this to work)
-        with open(fileName, 'a') as fileWriter:     #this is in append mode
+        if (not logFile.exists()):
+            logFolder.mkdir(parents=True)
+        with open(logFile, 'a') as fileWriter:     #this is in append mode
             if (not newline):
                 fileWriter.write('{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
                 fileWriter.write("\t")
@@ -17,43 +20,49 @@ class Logger:
             fileWriter.write(input)
             fileWriter.write("\n")
 
-    def compareAsset(self, weeklyLogPath:Path, compPath:Path, id, model, itemName, productNum, currQty, reorderAtQty, qtyToOrder, order) -> None:
-        #TODO: The current system will only log for asset creations and updates. We need to store the IDs in a list and run it afterwards to see if any assets were deleted
+    def compareAsset(self, weeklyLogFile:Path, compFile:Path, id, model, itemName, productNum, currQty, reorderAtQty, qtyToOrder, order) -> None:
         try:
-            if compPath.stat().st_size == 0:
-                self.logCreation(weeklyLogPath, id, model)
-                with open(compPath, 'a') as compareFile:
-                    # TODO: Write the asset to compPath
-                    ...
-            else:
-                compStr = ""
-                with open(compPath, 'r') as compFile:
-                    compStr = compFile.read()
-                print (compStr)
-                compJSON = json.loads(compStr)
-                print(compJSON)
+            if compFile.stat().st_size == 0:
+                # No compare file, need to create it
+                with open(weeklyLogFile, 'a') as weeklyLog:
+                    weeklyLog.write(f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tWeekly log created")
 
+            compStr:str = ""
+            with open(compFile, 'r') as compareFile:
+                compStr = compareFile.read()
+            print (compStr)
+            compJSON:dict = json.loads(compStr)
+            print(compJSON)
+
+            found:bool = False
             for asset in compJSON:
                 if asset['id'] == id:
-                    #TODO: Double check the field names are correct with what will actually be stored
+                    found = True
                     fieldList:list = ['model', 'itemName', 'productNum', 'currQty', 'reorderAtQty', 'qtyToOrder', 'order']
                     inputtedList:list = [model, itemName, productNum, currQty, reorderAtQty, qtyToOrder, order]
-
+                    
                     for field, input in zip(fieldList, inputtedList):
                         if asset[field] != input:
-                            self.logChange(weeklyLogPath, field, id, model, asset[field], input)
+                            self.logChange(weeklyLogFile, field, id, model, asset[field], input)
                             asset[field] = input
-                            with open(compPath, 'w') as compareFile:
+                            compJSON[id] = asset
+                            with open(compFile, 'w') as compareFile:
                                 compareFile.write(compJSON)
                     break
-
+            if not found:
+                # This was created
+                self.logCreation(weeklyLogFile, id, model, currQty)
+                with open(compFile, 'w') as compareFile:
+                    # The IDs are the keys, all fields are the values
+                    fields:dict = {'id': id, 'model': model, 'itemName': itemName, 'productNum': productNum, 'currQty': productNum, 'reorderAtQty': reorderAtQty, 'qtyToOrder': qtyToOrder, 'order': order}
+                    compJSON[id] = fields
         except Exception as ex:
             print(f"Compare Asset Error: {ex}")
             self.log(f"Compare Asset Error: {ex}")
 
-    def logCreation(self, weeklyLogPath, id, model) -> None:
+    def logCreation(self, weeklyLogPath:Path, id, model, qty) -> None:
         try:
-            logMsg:str = f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tAsset creation: ID: {id}, Name: {model}\n"
+            logMsg:str = f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tAsset creation: ID: {id}, Name: {model}, Quantity: {qty}\n"
             print(logMsg)
             with open(weeklyLogPath, 'a') as weeklyLog:
                 weeklyLog.write(logMsg)
@@ -72,6 +81,16 @@ class Logger:
         except Exception as ex:
             print(f"Error logging asset change ({fieldName}) for {model}: {ex}")
             self.log(f"Error logging asset change ({fieldName}) for {model}: {ex}")
+
+    def logDeletion(self, weeklyLogPath:Path, id, model, qty) -> None:
+        try :
+            logMsg:str = f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tAsset deletion: ID: {id}, Name: {model}, Quantity: {qty}"
+            print(logMsg)
+            with open(weeklyLogPath, 'a') as weeklyLog:
+                weeklyLog.write(logMsg)
+        except Exception as ex:
+            print(f"Error logging asset deletion for {model}: {ex}")
+            self.log(f"Error logging asset deletion for {model}: {ex}")
 
 if __name__ == '__main__':
     logger:Logger = Logger()
--- snipe_it_consumable_script.py
+++ snipe_it_consumable_script.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+import json
 from pathlib import Path
 from http_handler import HTTPHandler
 from asset_creator import AssetCreator
@@ -12,25 +13,28 @@ if __name__ == '__main__':
     handler:HTTPHandler = HTTPHandler()
     assetCreator:AssetCreator = AssetCreator(handler.getAsset())
     assets:list[Asset] = assetCreator.createAssets()
-    asset: Asset
+    asset:Asset
     logger.log("\n", True, True)
     logger.log("Begin script", True)
 
 
-    compFolderPath = Path("comparison")
-    compLogPath = Path("comparison/comparison.txt")
+    compFolderPath:Path = Path("comparison")
+    compLogPath:Path = Path("comparison/comparison.txt")
     if (not compLogPath.exists()):
         compFolderPath.mkdir(parents=True)
         with open(compLogPath, 'w') as compareFile:
             print("Comparison file created")
 
-    weeklyLogPath = Path("log/weekly-asset-log.txt")
+    weeklyLogPath:Path = Path("log/weekly-asset-log.txt")
     if (not weeklyLogPath.exists()):
         with open(weeklyLogPath, 'a') as file:
             file.write('{:%Y-%b-%d}'.format(datetime.datetime.now()))
 
+    currIDs:dict = {}
+
     for asset in assets:
         logger.compareAsset(weeklyLogPath, compLogPath, asset.id, asset.model, asset.itemName, asset.productNum, asset.currQty, asset.reorderAtQty, asset.qtyToOrder, asset.order)
+        currIDs[asset.id] = asset.id
         if asset.currQty <= asset.reorderAtQty:
             if asset.order == "":
                 if (asset.set_order("ORDERED")):
@@ -44,6 +48,18 @@ if __name__ == '__main__':
         elif asset.order != "":
             if (asset.set_order("")):
                 logger.log(f"Quantity now acceptable for asset {asset.productNum}, ORDERED tag was removed")
+
+
+    compStr:str = ""
+    with open(compLogPath, 'r') as compareFile:
+        compStr = compareFile.read()
+    compJSON:dict = json.loads(compStr)
+
+    # Log deleted assets
+    for oldID in compJSON:
+        if not currIDs[oldID]:
+            logger.logDeletion(weeklyLogPath, oldID, oldID['model'], oldID['currQty'])
+
     if False:
         #TODO: If it has been a week (tell by reading line), send the email (containing the weeklyLogFile) and remove the file ./log/weekly-asset-log.txt
         weeklyLogPath.unlink() #This deletes the file
