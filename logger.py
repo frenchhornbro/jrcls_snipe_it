e523f5ac9e33756600bdd6e894f31bdba89f5170
Hyrum Durfee <htdurfee@gmail.com>
2024-04-22 14:21:08 -0600

    Partial implementation of weekly log

--- logger.py
+++ logger.py
@@ -1,4 +1,6 @@
 import datetime
+import json
+from pathlib import Path
 
 class Logger:
     def __init__(self) -> None:
@@ -14,6 +16,62 @@ class Logger:
             if (not title): fileWriter.write("\t")
             fileWriter.write(input)
             fileWriter.write("\n")
+
+    def compareAsset(self, weeklyLogPath:Path, compPath:Path, id, model, itemName, productNum, currQty, reorderAtQty, qtyToOrder, order) -> None:
+        #TODO: The current system will only log for asset creations and updates. We need to store the IDs in a list and run it afterwards to see if any assets were deleted
+        try:
+            if compPath.stat().st_size == 0:
+                self.logCreation(weeklyLogPath, id, model)
+                with open(compPath, 'a') as compareFile:
+                    # TODO: Write the asset to compPath
+                    ...
+            else:
+                compStr = ""
+                with open(compPath, 'r') as compFile:
+                    compStr = compFile.read()
+                print (compStr)
+                compJSON = json.loads(compStr)
+                print(compJSON)
+
+            for asset in compJSON:
+                if asset['id'] == id:
+                    #TODO: Double check the field names are correct with what will actually be stored
+                    fieldList:list = ['model', 'itemName', 'productNum', 'currQty', 'reorderAtQty', 'qtyToOrder', 'order']
+                    inputtedList:list = [model, itemName, productNum, currQty, reorderAtQty, qtyToOrder, order]
+
+                    for field, input in zip(fieldList, inputtedList):
+                        if asset[field] != input:
+                            self.logChange(weeklyLogPath, field, id, model, asset[field], input)
+                            asset[field] = input
+                            with open(compPath, 'w') as compareFile:
+                                compareFile.write(compJSON)
+                    break
+
+        except Exception as ex:
+            print(f"Compare Asset Error: {ex}")
+            self.log(f"Compare Asset Error: {ex}")
+
+    def logCreation(self, weeklyLogPath, id, model) -> None:
+        try:
+            logMsg:str = f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tAsset creation: ID: {id}, Name: {model}\n"
+            print(logMsg)
+            with open(weeklyLogPath, 'a') as weeklyLog:
+                weeklyLog.write(logMsg)
+
+        except Exception as ex:
+            print(f"Error logging asset creation for {model}: {ex}")
+            self.log(f"Error logging asset creation for {model}: {ex}")
+
+    def logChange(self, weeklyLogPath, fieldName, id, model, prev, curr) -> None:
+        try:
+            logMsg:str = f"{'{:%a, %b-%d}'.format(datetime.datetime.now())}\tAsset change: ID: {id}, Name: {model}\n\tField: {fieldName}, Previous: {prev}, Current: {curr}\n"
+            print(logMsg)
+            with open (weeklyLogPath, 'a') as weeklyLog:
+                weeklyLog.write(logMsg)
+
+        except Exception as ex:
+            print(f"Error logging asset change ({fieldName}) for {model}: {ex}")
+            self.log(f"Error logging asset change ({fieldName}) for {model}: {ex}")
 
 if __name__ == '__main__':
     logger:Logger = Logger()
--- snipe_it_consumable_script.py
+++ snipe_it_consumable_script.py
@@ -1,4 +1,6 @@
 import time
+import datetime
+from pathlib import Path
 from http_handler import HTTPHandler
 from asset_creator import AssetCreator
 from asset import Asset
@@ -13,18 +15,36 @@ if __name__ == '__main__':
     asset: Asset
     logger.log("\n", True, True)
     logger.log("Begin script", True)
+
+
+    compFolderPath = Path("comparison")
+    compLogPath = Path("comparison/comparison.txt")
+    if (not compLogPath.exists()):
+        compFolderPath.mkdir(parents=True)
+        with open(compLogPath, 'w') as compareFile:
+            print("Comparison file created")
+
+    weeklyLogPath = Path("log/weekly-asset-log.txt")
+    if (not weeklyLogPath.exists()):
+        with open(weeklyLogPath, 'a') as file:
+            file.write('{:%Y-%b-%d}'.format(datetime.datetime.now()))
+
     for asset in assets:
-        if asset.get_current_qty() <= asset.get_reorder_at_qty():
-            if asset.get_order() == "":
+        logger.compareAsset(weeklyLogPath, compLogPath, asset.id, asset.model, asset.itemName, asset.productNum, asset.currQty, asset.reorderAtQty, asset.qtyToOrder, asset.order)
+        if asset.currQty <= asset.reorderAtQty:
+            if asset.order == "":
                 if (asset.set_order("ORDERED")):
-                    logger.log(f"Asset {asset.get_product_num()} was ordered")
+                    logger.log(f"Asset {asset.productNum} was ordered")
                     emailer:Emailer = Emailer(asset.email_msg())
                     emailer.run()
                     time.sleep(5) #Without this Jira freaks out and can take up to 30 minutes to actually receive the email and run its end of the script
-            elif asset.get_order() != "ORDERED":
+            elif asset.order != "ORDERED":
                 if (asset.set_order("")):
-                    logger.log(f"Order field for the asset {asset.get_product_num()} was cleared of the following unnecessary text: \"{asset.get_order()}\"")
-        elif asset.get_order() != "":
+                    logger.log(f"Order field for the asset {asset.productNum} was cleared of the following unnecessary text: \"{asset.order}\"")
+        elif asset.order != "":
             if (asset.set_order("")):
-                logger.log(f"Quantity now acceptable for asset {asset.get_product_num()}, ORDERED tag was removed")
+                logger.log(f"Quantity now acceptable for asset {asset.productNum}, ORDERED tag was removed")
+    if False:
+        #TODO: If it has been a week (tell by reading line), send the email (containing the weeklyLogFile) and remove the file ./log/weekly-asset-log.txt
+        weeklyLogPath.unlink() #This deletes the file
     logger.log("Script finished running")
