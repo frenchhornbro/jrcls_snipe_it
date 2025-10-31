151ec09e5b182c4e283e4940e1474b33d9427d19
frenchhornbro <htdurfee@gmail.com>
2025-10-31 11:02:27 -0600

    Refactored main logic into functions

--- snipe_it_consumable_script.py
+++ snipe_it_consumable_script.py
@@ -9,38 +9,41 @@ from asset import Asset
 from emailer import Emailer
 from logger import Logger
 
-if __name__ == '__main__':
-    logger:Logger = Logger()
-    logger.log("\n", True, True)
-    logger.log("START", True)
-    
-    # Get assets
-    handler:HTTPHandler = HTTPHandler()
-    currAssetJSON = handler.getAssets()
-    assetCreator:AssetCreator = AssetCreator(currAssetJSON)
-    assets:list[Asset] = assetCreator.createAssets()
+logger: Logger = Logger()
+compLogPath: Path = Path("./comparison/comparison.json")
+weeklyLogPath:Path = Path("./log/weekly-asset-log.txt")
 
-    # Create comparison and weekly log files
-    compChangeMade:bool = False
-    compLogPath:Path = Path("./comparison/comparison.json")
+def getAssets() -> list[Asset]:
+    handler: HTTPHandler = HTTPHandler()
+    currAssetJSON = handler.getAssets()
+    assetCreator: AssetCreator = AssetCreator(currAssetJSON)
+    return assetCreator.createAssets()
+
+def initCompFile() -> None:
     if not compLogPath.exists():
+        # Create comparison file
         os.makedirs(os.path.dirname(compLogPath), exist_ok=True)
-        with open(compLogPath, 'w') as compareFile:
+        with open(compLogPath, 'w'):
             logger.log("FILE:\tComparison file created")
 
-    weeklyLogPath:Path = Path("./log/weekly-asset-log.txt")
+def initWeeklyLogFile() -> None:
     if not weeklyLogPath.exists():
+        # Create weekly log file
         os.makedirs(os.path.dirname(weeklyLogPath), exist_ok=True)
-        with open(weeklyLogPath, 'w') as compareFile:
+        with open(weeklyLogPath, 'w'):
             pass
+    if os.path.getsize(weeklyLogPath) == 0:
+        # Write current date at head of weekly log file
+        with open(weeklyLogPath, 'w') as weeklyLog:
+            weeklyLog.write(f"{'{:%a, %b %d, %Y}'.format(datetime.now())}\n")
 
-    # Check if any existing assets are under minimum quantity
+def checkMinQuantity(assets: list[Asset]) -> None:
     for asset in assets:
         if asset.currQty <= asset.reorderAtQty:
             if asset.order == "":
                 if (asset.set_order("ORDERED")):
                     logger.log(f"ORDER:\tID: {asset.id}, Name: {asset.model}, Model No: {asset.productNum} - Order was placed")
-                    emailer:Emailer = Emailer(asset.email_msg())
+                    emailer: Emailer = Emailer(asset.email_msg())
                     emailer.run()
                     time.sleep(5) # Without this Jira freaks out and can take up to 30 minutes to actually receive the email and run its end of the script
             elif asset.order != "ORDERED":
@@ -50,19 +53,21 @@ if __name__ == '__main__':
             if (asset.set_order("")):
                 logger.log(f"UPDATE:\tQuantity now acceptable for asset {asset.productNum}, ORDERED tag was removed")
 
-    # Update the weekly log
-    currIDs:set = set()
+def getCurrIDs(assets: list[Asset]) -> tuple[set[str], bool]:
+    compChangeMade: bool = False
+    currIDs: set = set()
     for asset in assets:
         compChangeMade = compChangeMade or logger.compareAsset(weeklyLogPath, compLogPath, compChangeMade, str(asset.id), asset.model, asset.itemName, asset.productNum, str(asset.currQty), str(asset.reorderAtQty), str(asset.qtyToOrder), asset.order)
         currIDs.add(str(asset.id))
+    return currIDs, compChangeMade
 
-    # Log deleted assets
+def logDeletedAssets(currIDs: set[str]) -> bool:
+    compChangeMade: bool = False
     if os.path.getsize(compLogPath) != 0: # There will be no deleted assets if the compareFile is empty
-        compStr:str = ""
         with open(compLogPath, 'r') as compareFile:
-            compStr = compareFile.read()
-        compJSON:dict = json.loads(compStr)
-        delKeys:list[str] = []
+            compStr: str = compareFile.read()
+        compJSON: dict = json.loads(compStr)
+        delKeys: list[str] = []
         for oldID in compJSON.keys():
             if not oldID in currIDs:
                 asset = compJSON[oldID]
@@ -71,32 +76,43 @@ if __name__ == '__main__':
                 compChangeMade = True
         for key in delKeys:
             logger.deleteCompLogAsset(compLogPath, compJSON, key)
+    return compChangeMade
 
-    if os.path.getsize(weeklyLogPath) == 0:
-        with open(weeklyLogPath, 'w') as weeklyLog:
-            weeklyLog.write(f"{'{:%a, %b %d, %Y}'.format(datetime.now())}\n")
+def getEmailDeadline() -> None:
+    with open(weeklyLogPath, 'r') as weeklyLog:
+        dateString: datetime = weeklyLog.readline().strip()
+    return datetime.strptime(dateString, '%a, %b %d, %Y') + timedelta(days=7)
 
+def emailWeeklyLog() -> None:
+    # If the deadline has passed, compile the weekly asset log and email it to helpdesk@law.byu.edu
+    with open(weeklyLogPath, 'r') as weeklyLog:
+        weeklyLogText: list[str] = f"Start Date: {weeklyLog.read()}"
+    emailer: Emailer = Emailer(weeklyLogText, "Weekly Consumable Report")
+    emailer.run("printersupply@law.byu.edu","helpdesk@law.byu.edu")
+    # Delete the weekly asset log
+    weeklyLogPath.unlink()
+
+if __name__ == '__main__':
+    logger.log("\n", True, True)
+    logger.log("START", True)
+    assets: list[Asset] = getAssets()
+    # Create comparison and weekly log files
+    initCompFile()
+    initWeeklyLogFile()
+    # Send an email for each asset under minimum quantity
+    checkMinQuantity(assets)
+    # Update the weekly log with changes or deletions to assets
+    currIDs, compChangeMade = getCurrIDs(assets)
+    compChangeMade = logDeletedAssets(currIDs) or compChangeMade
+    # Add whitespace to log
     if compChangeMade:
         with open(weeklyLogPath, 'a') as weeklyLog:
             weeklyLog.write("\n")
-
+    # Send weekly email
     try:
-        # Check the deadline
-        dateString:datetime
-        with open(weeklyLogPath, 'r') as weeklyLog:
-            dateString = weeklyLog.readline().strip()
-        weeklyEmailDeadline = datetime.strptime(dateString, '%a, %b %d, %Y') + timedelta(days=7)
-        if datetime.now() > weeklyEmailDeadline:
-            # If the deadline has passed, compile the weekly asset log and email it to helpdesk@law.byu.edu
-            weeklyLogText:list[str] = "Start Date: "
-            with open(weeklyLogPath, 'r') as weeklyLog:
-                weeklyLogText += weeklyLog.read()
-            emailer:Emailer = Emailer(weeklyLogText, "Weekly Consumable Report")
-            emailer.run("printersupply@law.byu.edu","helpdesk@law.byu.edu")
-            # Delete the weekly asset log
-            weeklyLogPath.unlink()
+        if datetime.now() > getEmailDeadline():
+            emailWeeklyLog()
     except Exception as ex:
         print(f"Error checking the deadline to send the weekly log: {ex}")
         logger.log(f"ERROR:\tError checking the deadline to send the weekly log: {ex}")
-    
     logger.log("END", True)
